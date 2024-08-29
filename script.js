fafe0e3605c2751c57347019af0aef85752b77f0
tommyal47 <ahmedali.al047@gmail.com>
2024-08-29 15:55:59 +0300

    modify test pages

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const phoneNumber = document.getElementById('p_number');
 const year = document.getElementById('year');
 const sec1 = document.getElementById('f_page');
 const sec2 = document.getElementById('s_page');
+const sec3= document.getElementById('t_page');
+const sec4 = document.getElementById('fo_page');
+const sec5 = document.getElementById('fi_page');
+const sec6 = document.getElementById('se_page');
 
 // console.log(firstNext);
 
@@ -40,6 +44,26 @@ function sub (){
 
 
     
+}
+
+function section2 (){
+    sec2.classList.add('hide');
+    sec3.classList.remove('hide');
+}
+
+function section3 (){
+    sec3.classList.add('hide');
+    sec4.classList.remove('hide');
+}
+
+function section4 (){
+    sec4.classList.add('hide');
+    sec5.classList.remove('hide');
+}
+
+function section5 (){
+    sec5.classList.add('hide');
+    sec6.classList.remove('hide');
 }
 
 
